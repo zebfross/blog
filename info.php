d384fa19b9d0c44104462bcc9b521ee54cd2b515
Zeb Fross <zebfross@hotmail.com>
2016-10-02 12:14:52 -0700

    test

--- info.php
+++ info.php
@@ -1,3 +1,18 @@
-<?php
-phpinfo();
-?>
+<?php // @codingStandardsIgnoreFile
+
+// load dependencies unnecessary change
+require_once(dirname(__FILE__) . '/vendor/autoload.php');
+
+// instance Pico
+/*$pico = new Pico(
+    dirname(__FILE__),    // root dir
+    'config/',  // config dir
+    'plugins/', // plugins dir
+    'themes/'   // themes dir
+);
+
+// run application
+echo $pico->run();
+*/
+
+echo "cool beans";
